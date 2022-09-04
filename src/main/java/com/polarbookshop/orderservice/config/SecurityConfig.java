99c5c66d95857ba3e2d8a8aae25ac0330607c063
christophe Boschmans <cboschmans@protonmail.com>
2022-09-04 13:13:26 +0200

    adapted securityconfig

--- src/main/java/com/polarbookshop/orderservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/orderservice/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.polarbookshop.orderservice.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.actuate.info.InfoEndpoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -8,9 +11,16 @@ import org.springframework.security.web.server.savedrequest.NoOpServerRequestCac
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+
+    EndpointRequest.EndpointServerWebExchangeMatcher HEALTH_ENDPOINTS = EndpointRequest.to(HealthEndpoint.class, InfoEndpoint.class);
+
     @Bean
     SecurityWebFilterChain filterChain(ServerHttpSecurity httpSecurity){
-    return httpSecurity.authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+    return httpSecurity.authorizeExchange(exchange -> exchange
+                    .pathMatchers("/actuator/**").permitAll()
+                    .anyExchange().authenticated()
+            )
             .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
             .requestCache(requestCacheSpec-> requestCacheSpec.requestCache(NoOpServerRequestCache.getInstance()))
             .csrf(ServerHttpSecurity.CsrfSpec::disable)
